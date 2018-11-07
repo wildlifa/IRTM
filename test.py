89e66bd691cd6692457a46b1cd358804adacdba8
wildlifa <wildlifa@gmail.com>
2018-11-07 17:51:13 +0100

    added csv reader

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+import csv
+
+
 def main():
-    print("hello world")
+    with open('tweets.csv', newline='', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
 
 
 if __name__ == '__main__':
