399f2a8f228bac1b80da16593b07577764d5bbd9
wildlifa <wildlifa@gmail.com>
2018-11-08 01:52:57 +0100

    added some basic classes for data processing

--- CSVreaderClass.py
+++ CSVreaderClass.py
@@ -6,14 +6,18 @@ class CSVreader:
     def __init__(self):
         self.__raw_list = []
 
+    # turns an CSV file into a list of tuples which are sorted by ID
     def get_list_from_path(self, path):
         self.__raw_list = []
         with open(path, newline='', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter='\t')
-            doc_id_counter = 1
             for row in reader:
-                dataset = (doc_id_counter, row[0], row[1], row[2], row[3], row[4])
+                dataset = (row[0], row[1], row[2], row[3], row[4])
                 self.__raw_list.append(dataset)
-                doc_id_counter = doc_id_counter + 1
-        return self.__raw_list
+            self.__raw_list.sort(key=by_tweet_id)
+            return self.__raw_list
+
+
+def by_tweet_id(elem):
+    return elem[1]
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import CSVreaderClass
+import UnsortedTermListGeneratorClass
 
 
 # entry point of the interpreter
@@ -8,8 +9,13 @@ def main():
 
 # returns the dictionary and the inverted index list structure
 def index(filename):
-    reader = CSVreaderClass.CSVreader()
-    raw_list = reader.get_list_from_path(filename)
+    reader = CSVreaderClass.CSVreader()  # converter CSV to List
+    raw_list = reader.get_list_from_path(filename)  # returns a list sorted by tweet ids
+    unsorted_term_list_generator = UnsortedTermListGeneratorClass.UnsortedTermListGenerator()
+    unsorted_list = unsorted_term_list_generator.generate_from_list(raw_list)
+    unsorted_list.sort()  # sort the global term list
+    for each in unsorted_list:
+        print(each[0], each[1])
     dictionary = None
     list_structure = None
     return dictionary, list_structure
